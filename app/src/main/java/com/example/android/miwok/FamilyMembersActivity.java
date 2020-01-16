b6ded77b19e70bdfb9d0db4f5209fd84e7048da7
samuelpateixeira <samuelpateixeira@hotmail.com>
2020-01-16 16:48:39 +0000

    Im rebuilding the other activities by myself

--- app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PhrasesActivity extends AppCompatActivity {
+public class FamilyMembersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases);
+        setContentView(R.layout.activity_family_members);
     }
 }
